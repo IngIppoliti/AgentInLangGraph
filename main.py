48684d779c262a3d8145bf55122f5d93c6ace67f
IngIppoliti <ing.ippoliti@gmail.com>
2025-07-07 08:38:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,19 @@ def main():
     messages = [HumanMessage(content="What is the weather in sf?")]
     result = abot.graph.invoke({"messages": messages})
     
+    #LOG OF THE PREVIOUS GRAPH INVOCATION
+    #Calling: {'name': 'tavily_search_results_json', 'args': {'query': 'weather in San Francisco'}, 'id': 'call_PvPN1v7bHUxOdyn4J2xJhYOX'}
+    #Back to the model!
 
+    #SECOND EXAMPLE
+    #messages = [HumanMessage(content="What is the weather in sf and la?")]
+    #result = abot.graph.invoke({"messages": messages})
+
+    #LOG OF THE PREVIOUS GRAPH INVOCATION
+    #Calling: {'name': 'tavily_search_results_json', 'args': {'query': 'weather in San Francisco'}, 'id': 'call_1SqGYuEtOOFN1yiIHSQTPnvE'}
+    #Calling: {'name': 'tavily_search_results_json', 'args': {'query': 'weather in Los Angeles'}, 'id': 'call_8RiM72Y7G8V7c3HEEAML1SKP'}
+    #Back to the model!
+
+#AS you can see here before to go back to the model it performs 2 actions in the same transaction before to go to the model
 if __name__ == "__main__":
     main()
