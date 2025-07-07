aee6ccc66ed5c5b329cb8e52751966fc641511d2
IngIppoliti <ing.ippoliti@gmail.com>
2025-07-07 09:28:55 +0200

    Update state.py

--- agent_with_memory/state.py
+++ agent_with_memory/state.py
@@ -1,1 +1,32 @@
+from uuid import uuid4
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, AIMessage
 
+"""
+In previous examples we've annotated the `messages` state key
+with the default `operator.add` or `+` reducer, which always
+appends new messages to the end of the existing messages array.
+
+Now, to support replacing existing messages, we annotate the
+`messages` key with a customer reducer function, which replaces
+messages with the same `id`, and appends them otherwise.
+"""
+def reduce_messages(left: list[AnyMessage], right: list[AnyMessage]) -> list[AnyMessage]:
+    # assign ids to messages that don't have them
+    for message in right:
+        if not message.id:
+            message.id = str(uuid4())
+    # merge the new messages with the existing messages
+    merged = left.copy()
+    for message in right:
+        for i, existing in enumerate(merged):
+            # replace any existing messages with the same id
+            if existing.id == message.id:
+                merged[i] = message
+                break
+        else:
+            # append any new messages to the end
+            merged.append(message)
+    return merged
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], reduce_messages]
