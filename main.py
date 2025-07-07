ec589a6e702e5bbe40325ae2e459733e2bd34f73
IngIppoliti <ing.ippoliti@gmail.com>
2025-07-07 08:35:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ from langchain_openai import ChatOpenAI
 
 def main():
     model = ChatOpenAI(model="gpt-4", temperature=0)
+    prompt=                "You are a smart research assistant. Use the search engine to look up information. \
+                            You are allowed to make multiple calls (either together or in sequence). \
+                            Only look up information when you are sure of what you want. \
+                            If you need to look up some information before asking a follow up question, you are allowed to do that!"
+                        
     tools = [create_search_tool()]
-    agent = Agent(model=model, tools=tools, system="You are a helpful AI agent.")
-    agent.run()
+    agent = Agent(model=model, tools=tools, system=prompt)
+
+    #FIRST EXAMPLE
+    messages = [HumanMessage(content="What is the weather in sf?")]
+    result = abot.graph.invoke({"messages": messages})
+    
 
 if __name__ == "__main__":
     main()
