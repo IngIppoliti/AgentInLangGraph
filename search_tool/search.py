e8c3d2d2e8a88b3394f34b62d6b64a4c3935fd39
IngIppoliti <ing.ippoliti@gmail.com>
2025-07-07 08:59:27 +0200

    Update search.py

--- search_tool/search.py
+++ search_tool/search.py
@@ -1,3 +1,6 @@
+#Tavily Search API is a specialized search engine designed for Large Language Models (LLMs) and AI agents. It provides real-time, accurate, and unbiased information from the web.
+#the advantage is that it acts as a search engine accessing web updated information but return answer that are structured to be consumed bu agents (JSON format)
+
 from langchain_community.tools.tavily_search import TavilySearchResults
 
 def create_search_tool(max_results=4):
